b9d8cf5436ac49db442916034d99c1606a65a0e5
Amr Saher <79362856+AmrSaher@users.noreply.github.com>
2023-05-01 18:44:22 -0700

    Update HomeController.php

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -5,9 +5,6 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\View;
-use App\Models\User;
-use App\Models\SignUp;
-use App\Models\Invoice;
 
 class HomeController {
     public function index(): View
